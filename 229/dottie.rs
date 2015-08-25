7ffc7a649a5a8158376e806bffa3e0e0740a3169
Jeff Walters <sy.achmed@gmail.com>
2015-08-25 02:25:45 -0400

    [2015-08-24] Challenge #229 [Easy] The Dottie Number
    Adding challenges.  Moved fixed point loop into a lambda.

--- 229/dottie.rs
+++ 229/dottie.rs
@@ -1,8 +1,18 @@
 use std::f32;
 use std::io;
 
-fn f(x: f32) -> f32 {
-    x.cos()
+fn fixed_point<F>(f: F, mut x: f32) -> f32 
+    where F : Fn(f32) -> f32 {
+
+    let mut count = 0;
+    loop {
+        let old = x;
+        x = f(x);
+        count += 1;
+
+        if (x - old).abs() <= f32::EPSILON || count > 10000 { break; }
+    }
+    x
 }
 
 fn main() {
@@ -14,14 +24,29 @@ fn main() {
         .ok()
         .expect("invalid!");
 
-    let mut x: f32 = match x.trim().parse() {
+    let x: f32 = match x.trim().parse() {
         Ok(num) => num,
         Err(_) => { 1.1 },
     };
 
-    while (f(x) - x).abs() > f32::EPSILON {
-        x = f(x)
-    }
+    // Dottie Number
+    let dottie = fixed_point(|y| y.cos(), x);
+   
+    println!("Dottie: {}", dottie);
 
-    println!("Dottie: {}", x);
+
+    // Challenge 1
+    let ch1 = fixed_point(|y| y - y.tan(), x);
+
+    println!("challenge 1: {}", ch1);
+
+    // Challenge 2
+    let ch2 = fixed_point(|y| 1.0 + (1.0 / y), x);
+
+    println!("challenge 2: {}", ch2);
+
+    // Challenge 3
+    let ch3 = fixed_point(|y| 4.0 * y * (1.0 - y), x);
+
+    println!("challenge 3: {}", ch3);
 }
